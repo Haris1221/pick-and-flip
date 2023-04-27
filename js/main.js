4edb9e4b9321e8764d0c7c6e734e722255c315a6
haris <Haris199x@gmail.com>
2023-04-27 14:16:10 -0500

    FOURTH COMMIT

--- js/main.js
+++ js/main.js
@@ -1,73 +1,100 @@
 let cards = document.querySelectorAll('.cards')
-// let v = ["url('images/basketball.jpeg')","url('images/american football.jpeg')","url('images/baseball.jpeg')","url('images/soccer.jpeg')","url('images/volleyball.jpeg')","url('images/tennis.jpeg')","url('images/golf.jpeg')","url('images/handball.jpeg')","url('images/basketball.jpeg')","url('images/american football.jpeg')","url('images/baseball.jpeg')","url('images/soccer.jpeg')","url('images/volleyball.jpeg')","url('images/tennis.jpeg')","url('images/golf.jpeg')","url('images/handball.jpeg')" ]
+// cards.forEach(card => {
+//     card.style.backgroundImage = "https://images.pexels.com/photos/13962287/pexels-photo-13962287.png?auto=compress&cs=tinysrgb&w=300&lazy=load"
+// })
+
+
 let objects ={
-"american-football": "url('images/american-football.jpeg')",
-"baseball": "url('images/baseball.jpeg')",
-"basketball": "url('images/basketball.jpeg')",
-"golf" : "url('images/golf.jpeg')",
-"handball": "url('images/handball.jpeg')",
-"soccer": "url('images/soccer.jpeg')",
-"tennis": "url('images/tennis.jpeg')",
-"volleyball": "url('images/volleyball.jpeg')",
-"american-football": "url('images/american-football.jpeg')",
-"baseball": "url('images/baseball.jpeg')",
-"basketball": "url('images/basketball.jpeg')",
-"golf": "url('images/golf.jpeg')",
-"handball": "url('images/handball.jpeg')",
-"soccer": "url('images/soccer.jpeg')",
-"tennis": "url('images/tennis.jpeg')",
-"volleyball": "url('images/volleyball.jpeg')"
+    "american-football": "url('images/american-football.jpeg')",
+    "baseball": "url('images/baseball.jpeg')",
+    "basketball": "url('images/basketball.jpeg')",
+    "golf" : "url('images/golf.jpeg')",
+    "handball": "url('images/handball.jpeg')",
+    "soccer": "url('images/soccer.jpeg')",
+    "tennis": "url('images/tennis.jpeg')",
+    "volleyball": "url('images/volleyball.jpeg')",
+    "american-football": "url('images/american-football.jpeg')",
+    "baseball": "url('images/baseball.jpeg')",
+    "basketball": "url('images/basketball.jpeg')",
+    "golf": "url('images/golf.jpeg')",
+    "handball": "url('images/handball.jpeg')",
+    "soccer": "url('images/soccer.jpeg')",
+    "tennis": "url('images/tennis.jpeg')",
+    "volleyball": "url('images/volleyball.jpeg')"
 }
-
-
 let hasflipped = false
 let firstImage, secondImage
-
-
+let startButton = document.querySelector('#startButton')
+let rightGuesses = 0
+let lockBoard = false 
 
 // this function is going to change the class of the clicked card from 'card' to 'card-flip'.
 function cardFlip (e) {
+    if(lockBoard) return;
     e.preventDefault()
     this.style.removeProperty('backgroundImage')
-    this.style.backgroundColor = 'red'
-    this.classList.toggle('flip')
+    this.classList.add('flip')
 
 
     // this checks the first click as to whether it has been clicked before or not
     if(!hasflipped){
         hasflipped = true
         firstImage = this
+        console.log('this is first image inside card flip',firstImage)
     // this checks for the second click
     } else {
         hasflipped = false
         secondImage = this
+        console.log('this is second image inside card flip',secondImage)
         console.log(objects[firstImage.dataset.cards])
         console.log(objects[secondImage.dataset.cards])
+        cardMatch()
     }
-        if( secondImage !== undefined){
-        if(firstImage.dataset.cards === secondImage.dataset.cards){
-            // if the two images that were clicked are the same
-            firstImage.removeEventListener('click', cardFlip)
-            secondImage.removeEventListener('click', cardFlip)
-            console.log('done')
-        } else {
-            setTimeout(() => {
-                firstImage.classList.toggle('flip')
-                secondImage.classList.toggle('flip')
-            }, 1000);
-        }
+}
+
+
+function cardMatch() {
+if( secondImage !== undefined){
+    if(firstImage.dataset.cards === secondImage.dataset.cards){
+        // if the two images that were clicked are the same
+        twoMatched()
+    } else {
+        twoUnmatched()
     }
+}
+}
+
+function twoMatched () {
+    firstImage.removeEventListener('click', cardFlip)
+    secondImage.removeEventListener('click', cardFlip)
+    rightGuesses++
+    if(rightGuesses === 8) {
+        document.querySelector('h1').innerText = 'You Won'
     }
+}
+
+function twoUnmatched () {
+    lockBoard = true
+    setTimeout(() => {
+        firstImage.classList.remove('flip')
+        secondImage.classList.remove('flip')
+        lockBoard = false
+    }, 1000);
+    
+}
 
 // this loops throgh each individual clickable card and adds a click event listener for the card to flip if it has the 'card-flip' class.
 cards.forEach(card => {
     card.style.backgroundImage = objects[card.dataset.cards]
     card.style.backgroundSize = '80px 110px'
+    // if(cards.hasAttribute('.flip')) {
+    //     card.style.backgroundImage = objects[card.dataset.cards]
+    //     card.style.backgroundSize = '80px 110px'
+    // }
     // let flipped = document.querySelector('.cards.flip')
     card.addEventListener('click', cardFlip)
 })
 
-//countdown timer
 
 function startTimer(duration, display) {
 var timer = duration, minutes, seconds;
@@ -83,10 +110,13 @@ setInterval(function () {
     
     if (--timer < 0) {
         document.querySelector('#timerBox').innerText="00:00";
-    }
+        document.querySelector('h1').innerText = 'You Lost'
+    } 
 }, 1000);
 }
-
-var fiveMinutes = 90
+var oneMinute = 90
 display = document.querySelector('#timerBox');
-startTimer(fiveMinutes, display);
+
+startButton.addEventListener('click', function() {
+    startTimer(oneMinute, display)
+})
